4ddbad8dce90e5c6bef1786f8e9029950e753785
Harald Ott <>
2023-07-05 22:48:05 +0200

    fix test

--- provider.tf
+++ provider.tf
@@ -34,18 +34,3 @@ provider "aws" {
   }
 }
 
-resource "aws_iam_role" "invocation_role" {
-  name = "api_gateway_auth_invocation"
-  path = "/"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": {
-    "Effect": "Allow",
-    "Action": "s3:ListBucket",
-    "Resource": "arn:aws:s3:::test-bucket"
-  }
-}
-EOF
-}
--- tests/provider.tf
+++ tests/provider.tf
@@ -6,7 +6,3 @@ terraform {
     }
   }
 }
-
-provider "kubernetes" {
-  config_context_cluster   = "minikube"
-}
