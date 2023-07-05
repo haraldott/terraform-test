f8a40e8c0f4c25eae28adc4f76235bc317fc47f5
Harald Ott <>
2023-07-05 22:38:30 +0200

    test

--- main.tf
+++ main.tf
@@ -4,9 +4,6 @@ resource "aws_instance" "frontend" {
   ami = var.ami
   instance_type          = var.instance_type
   key_name               = "user1"
-  #monitoring             = true
-  #vpc_security_group_ids = ["sg-12345678"]
-  #subnet_id              = "subnet-eddcdzz4"
 
   tags = {
     Terraform   = "true"
@@ -20,9 +17,6 @@ resource "aws_instance" "backend" {
   ami = var.ami
   instance_type          = var.instance_type
   key_name               = "user1"
-  #monitoring             = true
-  #vpc_security_group_ids = ["sg-12345678"]
-  #subnet_id              = "subnet-eddcdzz4"
 
   tags = {
     Terraform   = "true"
@@ -36,9 +30,6 @@ resource "aws_instance" "matcher" {
   ami = var.ami
   instance_type          = var.instance_type
   key_name               = "user1"
-  #monitoring             = true
-  #vpc_security_group_ids = ["sg-12345678"]
-  #subnet_id              = "subnet-eddcdzz4"
 
   tags = {
     Terraform   = "true"
@@ -51,9 +42,6 @@ resource "aws_instance" "cruncher" {
   ami = var.ami
   instance_type          = var.instance_type
   key_name               = "user1"
-  #monitoring             = true
-  #vpc_security_group_ids = ["sg-12345678"]
-  #subnet_id              = "subnet-eddcdzz4"
 
   tags = {
     Terraform   = "true"
--- tests/main.tf
+++ tests/main.tf
@@ -1,16 +1,4 @@
 module "s3_test" {
   source = "../"
-
-  bucket = {
-    name = "test-bucket"
-  }
-
-  kms = {
-    admins = [
-      "*"
-    ]
-    access = [
-      "*"
-    ]
-  }
+  # tests go here
 }
